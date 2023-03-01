b0962f3e289fd09190b224708a4d0e02e42d17e1
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-02-28 23:54:11 -0300

    .

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -34,14 +34,17 @@ export function Header() {
                       title="Contato"
                       id={`offcanvasNavbarDropdown-expand-${expand}`}
                     >
-                      <NavDropdown.Item href="#action3">
+                      <NavDropdown.Item href="https://wa.me/5511982813448" target="_blank">
                         Whatsapp
                       </NavDropdown.Item>
-                      <NavDropdown.Item href="#action4">
+                      <NavDropdown.Item href="https://www.linkedin.com/in/caio-henrique-moraes-gon%C3%A7alves-5b4a74199/" target="_blank">
                         Linkedin
                       </NavDropdown.Item>
-                      <NavDropdown.Item href="#action5">
+                      <NavDropdown.Item href="mailto:caiohmg@gmail.com" target="_blank">
                         E-mail
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="https://github.com/caiohmg" target="_blank">
+                        Github
                       </NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
