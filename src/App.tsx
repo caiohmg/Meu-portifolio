48027d2ab89524c152db6bd06f0305fb31bde7cd
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-03-04 23:08:34 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Biography } from "./pages/Biography/Biography";
 import { Home } from "./pages/HomePages/home";
-import { Projects } from "./pages/projects";
-
-
 import { GlobalStyles } from "./styles/Global";
+import { Projects } from './pages/projects/index';
+import { Biography } from './pages/Biography/Biography';
 
 
 export function App() {
@@ -14,7 +12,8 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/biography" element={<Biography />} />
+        <Route path="/Biography" element={<Biography />} />
+       
       </Routes>
     </BrowserRouter>
   );
