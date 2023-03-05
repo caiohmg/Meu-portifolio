0ba4bcaed7056b396d20b4eff8fa205ff4942054
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-03-04 23:29:58 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/HomePages/home";
 import { GlobalStyles } from "./styles/Global";
 import { Projects } from './pages/projects/index';
-import { Biography } from './pages/Biography/Biography';
+import { Biography } from './pages/Biography';
 
 
 export function App() {
--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -5,6 +5,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { HeaderContainer } from "./styles";
 
+
 export function Header() {
   return (
     <HeaderContainer>
@@ -12,56 +13,40 @@ export function Header() {
         {["xl"].map((expand) => (
           <Navbar expand={expand}>
             <Container fluid>
-              <Navbar.Toggle
+              <Navbar.Toggle 
                 aria-controls={`offcanvasNavbar-expand-${expand}`}
               />
-              <Navbar.Offcanvas
+              <Navbar.Offcanvas 
                 id={`offcanvasNavbar-expand-${expand}`}
                 aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                placement="end"
+                placement="end" 
               >
                 <Offcanvas.Header className="custom-header" closeButton>
-                  <Offcanvas.Title
-                    id={`offcanvasNavbarLabel-expand-${expand}`}
-                  ></Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
+                  <Offcanvas.Title id=
+                  
+                  {`offcanvasNavbarLabel-expand-${expand}`}>
+                  </Offcanvas.Title>
+                </Offcanvas.Header >
+                <Offcanvas.Body >
                   <Nav className="justify-content-end">
-                    <a href="/" className="btn">
-                      Home
-                    </a>
-                    <a href="/projects" className="btn">
-                      Projetos
-                    </a>
-                    <a href="/biography" className="btn">
-                      Sobre mim
-                    </a>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="../projects">Projetos</Nav.Link>
+                    <Nav.Link href="../Biography">Sobre mim</Nav.Link>
                     <NavDropdown
+                      
                       title="Contato"
                       id={`offcanvasNavbarDropdown-expand-${expand}`}
                     >
-                      <NavDropdown.Item
-                        href="https://wa.me/5511982813448"
-                        target="_blank"
-                      >
+                      <NavDropdown.Item href="https://wa.me/5511982813448" target="_blank">
                         Whatsapp
                       </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="https://www.linkedin.com/in/caio-henrique-moraes-gon%C3%A7alves-5b4a74199/"
-                        target="_blank"
-                      >
+                      <NavDropdown.Item href="https://www.linkedin.com/in/caio-henrique-moraes-gon%C3%A7alves-5b4a74199/" target="_blank">
                         Linkedin
                       </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="mailto:caiohmg@gmail.com"
-                        target="_blank"
-                      >
+                      <NavDropdown.Item href="mailto:caiohmg@gmail.com" target="_blank">
                         E-mail
                       </NavDropdown.Item>
-                      <NavDropdown.Item
-                        href="https://github.com/caiohmg"
-                        target="_blank"
-                      >
+                      <NavDropdown.Item href="https://github.com/caiohmg" target="_blank">
                         Github
                       </NavDropdown.Item>
                     </NavDropdown>
@@ -75,3 +60,6 @@ export function Header() {
     </HeaderContainer>
   );
 }
+
+
+
