1ed5dbf119a561fde544edcefa4fb3e6826c7dea
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-03-04 23:26:00 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { Biography } from "./pages/Biography/Biography";
+
 import  { Header } from "./pages/Header";
-import { Projects } from "./pages/projects";
+
 
 
 
@@ -11,8 +11,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Header/>
     <App />
-    <Projects/>
-    <Biography/>
-    
+     
   </React.StrictMode>
 );
